71d2529fc4baf97334ff9ed846d8f2c93e5ba521
Vageesh Bhasin <vageesh_bhasin@outlook.com>
2016-02-28 23:35:57 -0700

    genesis

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neural Network Tutorial for MNIST database
+
+### References:
+* [MNIST Database of handwritten digits](http://yann.lecun.com/exdb/mnist)
+* [Neural Networks & Deep Learning Tutorial](http://neuralnetworksanddeeplearning.com/chap1.html)
---
+++ src/main.py
@@ -0,0 +1,12 @@
+import mnist_loader
+import network
+
+
+def main():
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network.Network([784, 100, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+
+if __name__ == "__main__":
+    main()
